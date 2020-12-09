ba57663bf4e1d78e74d6dceebf566e1762b5368d
Anders Jurisoo <jurisoo@hotmail.com>
2020-12-09 20:23:26 +0100

    Added InspectorTable

---
+++ resources/js/components/InspectorTable.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Diagram from '../Diagram';
+import { inject, observer } from "mobx-react"
+
+@inject('store') @observer
+export default class InspectorTable extends React.Component {
+    render() {
+        return (
+            <div className="flex flex-col">
+              <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                  <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table className="min-w-full divide-y divide-gray-200">
+                        {this.renderTableHead()}
+                        {this.renderTableBody()}
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+        );
+    }
+
+    renderTableHead() {
+        let headers = this.getHeaders()
+
+        return (
+            <thead>
+            <tr>
+                {this.getHeaders().map(heading => {
+                    return (
+                        <th key={heading} scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            {heading}
+                        </th>
+                    )
+                })}
+            </tr>
+          </thead>            
+        )
+    }
+
+    renderTableBody() {
+        return (
+            <tbody>
+                {this.getRows().map((row, rowIndex) => {
+                    return (
+                        <tr key={rowIndex} className="bg-white">
+                            {row.map((column, columnIndex) => {
+                                return (
+                                    <td key={columnIndex} className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-500">
+                                        {column}
+                                    </td>                            
+                                )
+                            })}
+                        </tr>
+                    )
+                })}
+
+                
+            </tbody>
+        )
+    }
+
+    getHeaders() {
+        return Object.keys(this.sample().users[0])
+    }
+
+    getRows() {
+        return this.sample().users.map(user => {
+            return Object.values(user)
+        })
+    }
+}
+
